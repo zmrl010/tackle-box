6d7868d0c5a8f2958be39125ed2406ea34bfdba9
Zach <zmriel@gmail.com>
2022-08-03 14:37:29 -0400

    chore!(config): cleanup for esm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,5 @@ require("@zmrl/eslint-config/patch/modern-module-resolution");
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["@zmrl"],
-  overrides: [
-    {
-      files: ["spec/*.ts?(x)"],
-      parserOptions: {
-        project: "spec/tsconfig.json",
-      },
-    },
-  ],
+  ignorePatterns: ["dist", "CHANGELOG.md"],
 };
--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
+export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "jest-environment-jsdom",
 };
